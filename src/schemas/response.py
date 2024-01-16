9c0b08add48ab2854c9dbe65b04808a2f4400f9d
Alexey <97658978+SAtbish@users.noreply.github.com>
2024-01-16 11:37:17 +0300

    change pydantic schemas

--- src/schemas/response.py
+++ src/schemas/response.py
@@ -1,5 +1,14 @@
 from src.schemas.base import ResponseModel
 from src.schemas.currency_pairs import CurrencyPairTime
+from pydantic import BaseModel, Field
+
+
+class AddedRows(BaseModel):
+    count: int = Field(gt=0)
+
+
+class AddedRowsResponse(ResponseModel):
+    data: AddedRows
 
 
 class AllSymbolsResponse(ResponseModel):
